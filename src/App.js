9813c887a6f91fa0f386aa69e67e217f845be91e
jihyeon <lovely2270@innogrid.com>
2020-08-26 14:53:58 +0900

    카테고리 분류 및 Router적용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import axios from 'axios';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+import NewsPage from './pages/NewsPage';
+import { Route, Router } from 'react-router-dom';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=84084ebfc9a74965bb5683e780be2020',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return <NewsList />;
+  // const [category, setCategory] = useState('all');
+  // const onSelect = useCallback((category) => setCategory(category), []);
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -22,15 +23,43 @@ const sampleArticle = {
   urlToImage: 'https://via.placeholder.com/160',
 };
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        const response = await axios.get(
+          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=84084ebfc9a74965bb5683e780be2020`,
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+  //대기중일때
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+
+  //아직 articles 값이 설정되지 않았을 때
+  if (!articles) {
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 };
